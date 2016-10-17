b19963fdae6a158f0a2b907b2143698b9c38e9db
Angel-mendoza <mendozaangel003@gmail.com>
2016-10-16 20:39:41 -0400

    listo como se muestra el index de productos

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
 	validates :description, presence: true, length: {minimum: 20}
 	validates :price, presence: true
 
-	has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :cover, styles: { medium: "", thumb: "400x300" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
 
 end
